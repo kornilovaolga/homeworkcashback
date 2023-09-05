7d880f0f3d22f1f8d33484902f6a37b99000e91a
Olga <kornilovaolga1488@gmail.com>
2023-09-05 18:36:58 +0300

    to commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+import org.testng.annotations.Test;
+import ru.netology.CashbackHackService;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
